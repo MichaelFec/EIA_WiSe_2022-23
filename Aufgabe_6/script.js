f323375d2141d0fb14f012204d6b4954657ff1b7
MichaelFec <115643358+MichaelFec@users.noreply.github.com>
2022-11-20 22:19:06 +0100

    zwischenstand

---
+++ Aufgabe_6/script.js
@@ -0,0 +1,5 @@
+const name = "Deutschland";
+const population = 83237000;
+console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in §{name} beträgt §{population}");
+let population = 83237000;
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_6/script.ts
@@ -0,0 +1,4 @@
+const name = "Deutschland";
+const population = 83237000;
+console.log("Die Gesamtzahl der Einwohnerinnen und Einwohner in §{name} beträgt §{population}")
+let population:number = 83237000;
