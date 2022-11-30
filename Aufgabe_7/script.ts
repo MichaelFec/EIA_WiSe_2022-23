216f183c11fb5a5d91a446dab5cb5e15d94a64eb
MichaelFec <115643358+MichaelFec@users.noreply.github.com>
2022-11-30 17:20:11 +0100

    abgabe

--- Aufgabe_7/script.ts
+++ Aufgabe_7/script.ts
@@ -1,7 +1,7 @@
-const germany = "Deutschland";
-const france = "Frankreich";
-const italy = "Italien";
-const spain = "Spanien"
+const germany: string = "Deutschland";
+const france: string = "Frankreich";
+const italy: string = "Italien";
+const spain: string = "Spanien"
 
 const populationGER2021 = 83240000;
 const populationGER2017 = 82790000;
@@ -17,20 +17,39 @@ let ProzentFRA = (1-populationFRA2017/populationFRA2021)*100;
 let ProzentITA = (1-populationITA2017/populationITA2021)*100;
 let ProzentSPA = (1-populationSPA2017/populationSPA2021)*100;
 
-console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + germany + " " + populationGER2021 + ".");
-console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + germany + " " + populationGER2017 + ".");
-console.log("In den vier Jahren ist die Einwohnerzahl in " + germany + " um " + ProzentGER + " % gestiegen.");
+let wholeEU = 447000000;
 
-console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + france + " " + populationFRA2021 + ".");
-console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + france + "" + populationFRA2017 + ".");
-console.log("In den vier Jahren ist die Einwohnerzahl in " + france + " um " + ProzentFRA + " % gestiegen.");
 
-console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + italy + " " + populationITA2021 + ".");
-console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + italy + " " + populationITA2017 + ".");
-console.log("In den vier Jahren ist die Einwohnerzahl in " + italy + " um " + ProzentITA + " % gesunken.");
+window.addEventListener("load", function(){
 
-console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + spain + " " + populationSPA2021 + ".");
-console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + spain + " " + populationSPA2017 + ".");
-console.log("In den vier Jahren ist die Einwohnerzahl in " + spain + " um " + ProzentSPA + " % gestiegen.");
 
+    document.querySelector(".germany").addEventListener("click", function() {population(germany, populationGER2021, ProzentGER, populationGER2017)} )
+    document.querySelector(".france").addEventListener("click", function() {population(france, populationFRA2021, ProzentFRA, populationFRA2017)} )
+    document.querySelector(".spain").addEventListener("click", function() {population(spain, populationSPA2021, ProzentSPA, populationSPA2017)} )
+    document.querySelector(".italy").addEventListener("click", function() {population(italy, populationITA2021, ProzentITA, populationITA2017)} )
+
+    function population(_countryName: string, _countryPopulation21: number, _countryGrowth: number, _countryPopulation17: number) {
+        document.querySelector ("#countryName").innerHTML = _countryName;
+        document.querySelector ("#countryPopulation").innerHTML = _countryPopulation21.toString();
+        document.querySelector("#populationRelative").innerHTML = Math.round(_countryPopulation21/wholeEU*100*100) / 100 + "%";
+        document.querySelector("#populationGrowth").innerHTML = Math.round(_countryGrowth) + "%";
+        document.querySelector("#growthRate").innerHTML = Math.round((_countryPopulation21 - _countryPopulation17)) + "";
+
+    }
+    console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + germany + " " + populationGER2021 + ".");
+    console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + germany + " " + populationGER2017 + ".");
+    console.log("In den vier Jahren ist die Einwohnerzahl in " + germany + " um " + ProzentGER + " % gestiegen.");
+
+    console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + france + " " + populationFRA2021 + ".");
+    console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + france + "" + populationFRA2017 + ".");
+    console.log("In den vier Jahren ist die Einwohnerzahl in " + france + " um " + ProzentFRA + " % gestiegen.");
+
+    console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + italy + " " + populationITA2021 + ".");
+    console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + italy + " " + populationITA2017 + ".");
+    console.log("In den vier Jahren ist die Einwohnerzahl in " + italy + " um " + ProzentITA + " % gesunken.");
+
+    console.log("Im Jahre 2021 betrug die Einwohnerzahl in " + spain + " " + populationSPA2021 + ".");
+    console.log("Im Jahre 2017 betrug die Einwohnerzahl in " + spain + " " + populationSPA2017 + ".");
+    console.log("In den vier Jahren ist die Einwohnerzahl in " + spain + " um " + ProzentSPA + " % gestiegen.");
+});
 
